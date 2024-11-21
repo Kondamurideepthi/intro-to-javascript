11dd0f56308e85dd4285c3f045bdaee6938d4149
Deepthi Kondamuri <117149502+Kondamurideepthi@users.noreply.github.com>
2024-11-21 16:11:19 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,12 @@
+let arrayitems=[1,"hii",4.5,true,"hello"];
+console.log(arrayitems);
+console.log(arrayitems[0]);
+console.log(arrayitems[5]);
+console.log(arrayitems.push("helloo"));
+//console.log(arrayitems.pop())
+//let popitem= arrayitems.pop();
+//console.log(popitem);
+console.log(arrayitems.length);
+console.log(arrayitems);
+
+
